e41b94c7c0f870c810b8868649056405ed17511c
NV4RE <tosbodesz@gmail.com>
2020-11-13 23:32:41 +0700

    Update group struct

--- models/group.go
+++ models/group.go
@@ -1,7 +1,25 @@
 package models
 
+import "time"
+
 type Group struct {
-	Channel
+	ID    string `json:"_id"`
+	Name  string `json:"name"`
+	Fname string `json:"fname,omitempty"`
+	Type  string `json:"t"`
+	Msgs  int    `json:"msgs"`
+
+	ReadOnly  bool `json:"ro,omitempty"`
+	SysMes    bool `json:"sysMes,omitempty"`
+	Default   bool `json:"default"`
+	Broadcast bool `json:"broadcast,omitempty"`
+
+	Timestamp *time.Time `json:"ts,omitempty"`
+	UpdatedAt *time.Time `json:"_updatedAt,omitempty"`
+
+	User        *User    `json:"u,omitempty"`
+	LastMessage *Message `json:"lastMessage,omitempty"`
+
 	Users []string `json:"usernames"`
 }
 
